bf36db1f0e5b69c58c48adf407cf5db6311b7712
Aaron Stone <apstone@protonmail.com>
2021-10-01 23:33:07 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      v-bind:class="{ loading: loading }"
+      class="pokeball"
+      alt="Pokeball"
+      src="./assets/pokeball.png"
+      width="250"
+    />
+    <div v-if="!loading" class="pokemon-container">
+      <div v-for="monster in pokemon" :key="monster.id" class="pokemon">
+        <img alt="Pokemon Image" v-bind:src="monster.sprites.front_default" />
+        <h2>{{ monster.name }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const Pokedex = require("pokeapi-js-wrapper");
+const customOptions = {
+  cache: true,
+  cacheImages: true,
+};
+const P = new Pokedex.Pokedex(customOptions);
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function() {
+    return {
+      pokemon: [],
+      loading: true,
+    };
+  },
+  components: {},
+  methods: {
+    fetchPokemon: async function() {
+      const response = await P.resource("/api/v2/pokedex/2/");
+      const pokemonEntries = response.pokemon_entries;
+      const pokemonPromises = pokemonEntries.map((entry) => {
+        const name = entry.pokemon_species.name;
+        return P.getPokemonByName(name);
+      });
+
+      const pokemon = await Promise.all(pokemonPromises);
+
+      return pokemon;
+    },
+  },
+  mounted: function() {
+    this.fetchPokemon().then((data) => {
+      this.pokemon = data;
+      this.loading = false;
+    });
+  },
+};
 </script>
 
 <style>
@@ -24,5 +64,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.pokemon-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.pokemon {
+  padding: 20px;
+  width: 150px;
+}
+.pokemon h2 {
+  text-transform: capitalize;
+}
+.pokeball.loading {
+  animation: spin 2s infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
