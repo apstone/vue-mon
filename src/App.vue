5633dc85a1e0a0e5718bc70a009c1bdbb82899bb
Aaron Stone <apstone@protonmail.com>
2021-10-02 19:39:14 -0400

    Change fonts

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,6 @@ html {
   color: #f8f8f6;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
